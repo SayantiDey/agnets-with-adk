fa53ffdcb936f6c1a82959e6aceb62b0397b036b
Sayanti2.8 <42462352+SayantiDey@users.noreply.github.com>
2025-07-02 16:29:25 +0530

    Update agent.py

--- tech_assistant/tech-assistant/agent.py
+++ tech_assistant/tech-assistant/agent.py
@@ -1,1 +1,12 @@
+from google.adk.agents import Agent
 
+from .prompt import agent_prompt
+from .tools.tools import brainstorming_tool, troubleshooting_tool, toolbox_tools
+
+
+root_agent = Agent(
+    model="gemini-2.5-flash",
+    name="technical_assistant",
+    instruction=agent_prompt,
+    tools=[brainstorming_tool, troubleshooting_tool, *toolbox_tools],
+)
